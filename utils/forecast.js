b90e2093816b7a98703716448d02077fec083ffa
Renan Malato <malatows@gmail.com>
2024-05-31 13:45:47 -0400

    humidity

--- utils/forecast.js
+++ utils/forecast.js
@@ -13,8 +13,9 @@ const forecast = (longitude, latitude, callback) => {
         } else if (body.error) {
            callback( 'Unable to find location. Try another search', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out ' + body.current.humidity + ' degrees out.')  
-        }
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out ' + body.current.humidity + ' humidity')  
+            console.log(body)
+        }   
     
     })
 }
